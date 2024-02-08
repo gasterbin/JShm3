29862508e4917143ce8ffca27f2b1f3fa8294725
gasterbin <tergabi2007@gmail.com>
2024-02-08 21:23:26 +0200

    !

---
+++ src/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const COUNT_OF_BALLS = 12;
+const DIAMETER = 0.5;
+const THICKNESS = 0.01;
+let volumeOfSmallestBall = 0;
+let commonVolume = 0;
+let currentVolume = 0;
+let currentDiameter = 0;
+
+volumeOfSmallestBall = (Math.PI * DIAMETER ** 3) / 6;
+
+for (let i = 1; i <= COUNT_OF_BALLS; i++) {
+  currentDiameter = DIAMETER + 2 * (i - 1) * THICKNESS;
+  currentVolume = (Math.PI * currentDiameter ** 3) / 6;
+  commonVolume += currentVolume;
+}
+
+console.log(
+  `The volume of ${COUNT_OF_BALLS} ball(s) with thickness is ${commonVolume} meters^3`
+);
+
